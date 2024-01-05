978edace1c4b0ade5551a4bf502487eb5f68bd86
Kingsley Ihemelandu <kijuchihe@gmail.com>
2024-01-06 00:17:48 +0100

    Start setting up errorHandler

---
+++ src/v1/models/userModel.ts
@@ -0,0 +1,16 @@
+import mongoose,{Schema, Document} from "mongoose";
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema: Schema = new Schema({
+    username: {},
+    email: {},
+    password: {}
+})
+
+export default mongoose.model<User>("User", UserSchema)
+
---
+++ src/v1/services/userService.ts
@@ -0,0 +1,12 @@
+import UserModel from "../models/userModel";
+
+export class UserService {
+    async getUsers():Promise<any> {
+        try {
+            const users = await UserModel.find();
+            return users
+        } catch (error) {
+            throw new Error(``)
+        }
+    }
+}
